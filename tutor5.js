c940a4d55ae02da7819f2e94711f5eb0036524df
syfanialya <syfanialyafauziyyah@gmail.com>
2020-01-31 20:49:12 +0700

    modul5

---
+++ tutor5.js
@@ -0,0 +1,71 @@
+//1
+var variabel1 = '23'
+var variabel2 = 23
+
+//2
+let variabel3 = '34'
+let variabel4 = 34
+
+//3
+variabel5 = '65'
+variabel6 = 65
+
+//1
+function namaFungsi(){
+
+}
+
+//2
+var tambah = function(a, b){
+    return a + b
+}
+tambah(2, 3)
+
+//3
+var kurang = (a, b) => {
+    return a - b
+}
+kurang(1, 2)
+
+//1
+x = 6
+if(x < 7){
+
+}else if(x > 9){
+
+}else{
+
+}
+
+//3
+x < 4 ? 'True' : 'False'
+
+x < 7 ? 'True' : x > 9 ? 'True' : 'False'
+
+//2
+switch(x){
+    case 5 : 'Lima'
+    case 6 : 'Enam'
+    default : 'Else'
+}
+
+//1
+for(let i=0;i<10;i++){}
+    console.log(array[i])
+array = [2, 3, 4, 5, 6]
+
+//2
+for (item in array)
+    console.log(item)
+
+//3
+while(x < 3){
+
+}
+
+//4
+do {
+    
+}while(x < 3){
+    
+}
